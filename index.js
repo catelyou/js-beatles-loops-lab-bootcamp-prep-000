39fae66a0a9f8279bb052a277c82c21234dde13c
Jaclyn Hall <jaclynchall@gmail.com>
2018-01-14 00:30:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,13 +7,12 @@ function theBeatlesPlay(players, instruments) {
 
 function johnLennonFacts() {
   var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
-  var newFacts = [];
   var i = 0;
   while (i < facts.length) {
-        newFacts.push(facts[i] +"!!!");
+        facts.push(facts[i] +"!!!");
         i++;
      }
-  return newFacts;
+  return facts;
   }
 
 function iLoveTheBeatles(n) {
